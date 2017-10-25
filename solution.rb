6b227082f47d4f889f00e5a52007a6ab26343757
Edgardo David <edgardo.burgos94@gmail.com>
2017-10-24 21:58:39 -0500

    Solución reto 2 http y sinatra

---
+++ solution.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+get '/makers/:nombre' do
+
+  if params['nombre'].empty?
+    @saludame = "Hola desconocido"
+  else
+    @saludame = "Hola #{params['nombre'].capitalize}"
+  end
+
+  erb :index
+end
