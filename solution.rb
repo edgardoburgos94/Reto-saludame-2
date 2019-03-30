75c9aa1cc06f451a19cbb00aa6ed3426656b3302
edgardoburgos94 <32230637+edgardoburgos94@users.noreply.github.com>
2019-03-29 19:50:33 -0500

    ...

--- solution.rb
+++ solution.rb
@@ -5,7 +5,7 @@ get '/makers/:nombre' do
 #  if params['nombre'].empty?
 #    @saludame = "Hola desconocido"
 #  else
-    @saludame = "Hola #{params['nombre'].capitalize}"
+    @saludame = "Hola #{params[:nombre].capitalize}"
 #  end
 
   erb :index
